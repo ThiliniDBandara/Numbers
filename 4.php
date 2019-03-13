303ab5932b7358938c35b586c4eb9f9ce1ec9512
thilinitd <thilini.td@gmail.com>
2019-03-13 11:21:01 +0530

    NUmbers

---
+++ 4.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Numbers</title>
+</head>
+<body>
+<?php
+
+$num1=25;
+$num2=5;
+
+echo $num1+$num2;//addition
+echo"<br/>";
+
+echo $num1-$num2;//subtraction
+echo"<br/>";
+
+echo $num1*$num2;//multiplication
+echo"<br/>";
+
+echo $num1/$num2;//division
+echo"<br/>";
+
+echo $num1++;//post-increment
+echo"<br/>";
+
+echo $num2--;//post-decrement
+echo"<br/>";
+
+echo ++$num1;//pre-increment
+echo"<br/>";
+
+echo --$num2;//post-decrement
+echo"<br/>";
+
+?>
+
+</body>
+</html>
